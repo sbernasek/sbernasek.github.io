fe7ea9859e2b2ccbd952427ed5c000b727405118
Sebastian Bernasek <sebastian@u.northwestern.edu>
2020-04-04 19:19:16 -0700

    Site updated at 2020-04-05 02:19:15 UTC

---
+++ code/templating/post.py
@@ -0,0 +1,75 @@
+from os.path import join
+import pystache
+
+
+class ImageHtmlTemplate(object):
+
+    def __init__(self, url, caption=None, gps=None):
+        self._url = url
+        if caption is None:
+            caption = 'PLACEHOLDER_CAPTION'
+        self._caption = caption
+        self._hyperlink = self.build_hyperlink(gps)
+
+    @staticmethod
+    def build_hyperlink(gps):
+        if None in gps:
+            return ''
+        else:
+            query = 'https://www.google.com/maps/search/?api=1&query={:3.7f},{:3.7f}'.format(*gps)
+            return 'href="{:s}"'.format(query)
+
+    def url(self):
+        return self._url
+
+    def caption(self):
+        return self._caption
+
+    def hyperlink(self):
+        return self._hyperlink
+
+
+class PostTemplate(object):
+
+    def __init__(self, title, records, cover=None, subtitle=None):
+        self._title = title
+        self._subtitle = subtitle
+        self._cover = cover
+        self.records = records
+        self.renderer = pystache.Renderer()
+
+    def title(self):
+        return self._title
+
+    def subtitle(self):
+        return self._subtitle
+
+
+
+    def images(self):
+        html_strings = self.records.apply(self.render_image_html, axis=1)
+        return '\n\n'.join(html_strings)
+
+    def cover(self):
+        if self._cover is None:
+            return self.records.iloc[0].imgur_link
+        else:
+            return self._cover
+
+    def render_image_html(self, record):
+        image = ImageHtmlTemplate(record.imgur_link, gps=record.gps)
+        return self.renderer.render(image)
+
+
+class Post:
+    
+    posts_dir = '../_posts' 
+    
+    def __init__(self, filename, title, records, cover=None):
+        self.filepath = join(self.posts_dir, '{:s}.md'.format(filename))
+        template = PostTemplate(title, records, cover=cover)
+        self.post = pystache.Renderer().render(template)
+        
+    def write(self):
+        with open(self.filepath, 'w') as file:
+            file.write(self.post)
